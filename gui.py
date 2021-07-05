972e64e04505c3b13f2035db097b5f7485716474
SebasGamboa10 <segamboachacon@gmail.com>
2021-07-04 21:10:11 -0600

    [ADD] Python & Vagrant integration

--- gui.py
+++ gui.py
@@ -3,10 +3,14 @@ from fabric.api import *
 
 @task
 def mytask():
-    run('echo $USER')
+    
+    run('sudo verible/bazel-bin/verilog/tools/lint/verible-verilog-lint /vagrant_data/utils/sample8.v >log.txt')
+    run('ls')
 
 v = vagrant.Vagrant()
 v.up()
 env.hosts = [v.user_hostname_port()]
 env.key_filename = v.keyfile()
+env.warn_only = True
+
 execute(mytask)
