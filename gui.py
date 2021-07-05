c6d8e97513c9e03a594fc2690c21eeb8d93cdebe
Josue Araya García <jdag98228@gmail.com>
2021-07-04 23:27:03 -0600

    Solución de interfaz para visualizar log.txt

--- gui.py
+++ gui.py
@@ -1,16 +1,79 @@
-import vagrant
-from fabric.api import *
+#IMPORTS
+#import os
+import os
+from sys import modules
+from tkinter import *
+from tkinter import Menu
+from tkinter import filedialog
+from pathlib import Path
 
-@task
-def mytask():
-    
-    run('sudo verible/bazel-bin/verilog/tools/lint/verible-verilog-lint /vagrant_data/utils/sample8.v >log.txt')
-    run('ls')
 
-v = vagrant.Vagrant()
-v.up()
-env.hosts = [v.user_hostname_port()]
-env.key_filename = v.keyfile()
-env.warn_only = True
+#GUI
+root = Tk()
+root.title("GUI Mark I")
+root.geometry("1100x650")
+fileName = ""
 
-execute(mytask)
+#Functions
+def clear():
+    text.delete(1.0,END)
+
+def open_txt():
+    text.delete(1.0,END)
+    text_file = filedialog.askopenfilename(title="Open File")
+    fileName = text_file
+    print(text_file)
+    modules = open(text_file,'r')
+    text.insert(END,modules.read())
+    modules.close()
+
+def save_txt():
+    #text_file = filedialog.askopenfilename(title="Open File")
+    text_file = filedialog.asksaveasfilename(defaultextension=".v",title="Save File")
+    text_file = open(text_file,'w')
+    text_file.write(text.get(1.0,END))
+    text_file.close()
+
+def syntesis_txt():
+    scriptLine = 'python run.py '
+    os.system(scriptLine+fileName)
+    #text_file = open("C:/Users/toshi/Downloads/Level up/VLSI-Express-Chip-Design/log.txt",'r')
+    text_file= open("log.txt",'r')
+    result.insert(END,text_file.read())
+    text_file.close()
+
+#Top Menu
+menu = Menu(root)
+
+menu_file = Menu(menu,tearoff=0)
+menu_file.add_command(label="New File")
+menu_file.add_command(label="Save")
+
+menu_run = Menu(menu,tearoff=0)
+menu_run.add_command(label="Run")
+
+menu.add_cascade(label="File", menu=menu_file)
+menu.add_cascade(label="Run", menu=menu_run)
+root.config(menu=menu)
+
+#Text Input
+text = Text(root, width=30,height=20,font=("Helvetica",16));
+text.grid(row=0,column=0,pady=20, padx=20)
+result = Text(root, width=30,height=20,font=("Helvetica",16));
+result.grid(row=0,column=5,pady=20)
+
+clear_button = Button(root,text="Clear Screen",command=clear)
+clear_button.grid(row=5,column=1,pady=20)
+
+open_button = Button(root,text="Open Text File",command=open_txt)
+open_button.grid(row=5,column=2,pady=20)
+
+save_button = Button(root,text="Save File",command=save_txt)
+save_button.grid(row=5, column=3, pady=20)
+
+syntesis_button = Button(root,text="Syntesis File",command=syntesis_txt)
+syntesis_button.grid(row=5, column=4 , pady=20)
+
+
+#Main Loop
+root.mainloop()
