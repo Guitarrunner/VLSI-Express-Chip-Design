f2896dfafa241d07e1a1fbac72260c1b1bcf609e
Racso08 <oscar080899@gmail.com>
2021-07-04 22:08:37 -0600

    [ADD] Some details to run.py

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
-# Run command: python3 run.py
+# Run command: python3 run.py filename.v 
+# NOTE: Remenber that the file needs to be in the utils directory
 
 # Imports
 import vagrant
 from fabric.api import *
 import sys
+import os
 
 # Functions
 @task
@@ -11,8 +13,12 @@ def mytask():
     scriptLine = 'sh script.sh '
     fileName = sys.argv[1]
     run(scriptLine+fileName)
+    print("Exit Vagrant\n")
+    print("-------------------- Analysis Result --------------------")
+    os.system('cat log.txt')
 
 # Script
+print("Starting Vagrant\n")
 v = vagrant.Vagrant()
 v.up()
 env.hosts = [v.user_hostname_port()]
