2af17b89aecbe5fdc73d7614f0a1d4f1b98979d5
Racso08 <oscar080899@gmail.com>
2021-07-25 19:45:26 -0600

    [DELETE] Prints lines in previous update

--- scriptMaster.py
+++ scriptMaster.py
@@ -758,13 +758,13 @@ try:
     # Current tag version
     repo = git.Repo(search_parent_directories = True)
     repoTag = repo.tags[-1]
-    print(repoTag)           
+    #print(repoTag)           
 
     # Remote current tag version
     g = git.cmd.Git()
     blob = g.ls_remote('https://github.com/Guitarrunner/VLSI-Express-Chip-Design', sort='-v:refname', tags=True)
     remoteTag = blob.split('\n')[0].split('/')[-1]  
-    print(remoteTag)                      
+    #print(remoteTag)                      
 
     if repoTag != remoteTag:
         print("[INFO] There is a new update.")
